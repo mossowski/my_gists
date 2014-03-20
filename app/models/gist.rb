a667d05b29ab31aed0fe425ceea2787e6f345712
mossowski <lcinek18@gmail.com>
2014-03-20 15:55:33 +0100

    select + poprawki

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,6 +1,8 @@
 class Gist < ActiveRecord::Base
 	validates :snippet, presence: true,
 	                    length: { minimum: 3 }
+	validates :description, presence: true,
+	                    length: { minimum: 1 }
 
   def self.search(search)
   	if search
